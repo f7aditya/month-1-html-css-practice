e58e88327bc00c3a2abd2b221d37f2bc917277d5
f7aditya <adiverma8858@gmail.com>
2026-02-21 01:28:35 +0530

    day 7: implemented promises and async await delay function

---
+++ javascript/day2.js
@@ -0,0 +1,16 @@
+// async-await
+
+function setTimeoutPromisified(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function solve() {
+  await setTimeoutPromisified(1000);
+  console.log("hi");
+  await setTimeoutPromisified(3000);
+  console.log("hello");
+  await setTimeoutPromisified(5000);
+  console.log("hi there");
+}
+
+solve();
